825e0c8e2f7edbb9e9edca5a5f8fd96d10d5fc67
Mahmoud Mamdouh Abd Allah <49378579+Mahmoud-Mamdouh-Abdullah@users.noreply.github.com>
2020-05-04 03:33:42 +0200

    week4 work and assignment 4

---
+++ js testing/js/script.js
@@ -0,0 +1,10 @@
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.bark = function () {
+  console.log(this.name + " likes barking! Bark!");
+}
+
+var max = new Dog("Max", "Buddy");
+max.bark();
